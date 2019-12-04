1bc4ea03283b3c1ca7dc8ff04843fd067c2fee73
Federico Villa <fedevilla88@gmail.com>
2019-12-04 13:31:26 -0300

    Adding Expires time

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -29,7 +29,9 @@ router.post("/signup", async (req, res) => {
     const savedUser = await user.save();
 
     // ASSIGN TOKEN
-    const token = jwt.sign({ _id: savedUser._id }, process.env.SECRET_TOKEN);
+    const token = jwt.sign({ _id: savedUser._id }, process.env.SECRET_TOKEN, {
+      expiresIn: "24h"
+    });
     res.send({
       token,
       _id: savedUser._id,
