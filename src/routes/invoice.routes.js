b830fc533814d83b8006ed1136cd3400f464cfc2
Federico Villa <fedevilla88@gmail.com>
2019-12-16 14:08:56 -0300

    Fixing file name

--- src/routes/invoice.routes.js
+++ src/routes/invoice.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 
 const Invoice = require("../models/invoice");
 const isAuth = require("../middlewares/isAuth");
