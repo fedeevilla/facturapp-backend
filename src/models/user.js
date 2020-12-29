ca6daa4143308eee38d2a0a8d8142d7b57255422
Federico Villa <fedevilla88@gmail.com>
2020-12-29 11:28:39 -0300

    Added isPremiun into User Schema

--- src/models/user.js
+++ src/models/user.js
@@ -14,6 +14,7 @@ const UserSchema = new Schema({
   usdBalance: { type: Number, default: 0 },
   usdBankUS: { type: Number, default: 0 },
   usdBankAR: { type: Number, default: 0 },
+  isPremiun: { type: Boolean, default: false },
 });
 
 UserSchema.methods.toJSON = function () {
